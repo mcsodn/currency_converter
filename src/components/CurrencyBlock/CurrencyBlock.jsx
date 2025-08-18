aa24e72b1c0d14a543ce2b9d2747d29d0a501374
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-08-18 20:34:50 +0500

    add one currency block component

---
+++ src/components/CurrencyBlock/CurrencyBlock.jsx
@@ -0,0 +1,13 @@
+import './CurrencyBlock.css';
+
+const CurrencyBlock = () => {
+    return <div className='currency-block'>
+        <input type="text" name="" id="" value={'100,134'} />
+        <select name="" id="" >
+            <option value="eur">EUR</option>
+        </select>
+        <p>course</p>
+    </div>
+}
+
+export default CurrencyBlock;
