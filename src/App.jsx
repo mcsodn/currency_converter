6265964287f381e13e750687089faccd90713de9
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-08-19 20:56:26 +0500

    fix API props

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
 import './App.css';
 import HeaderElement from "./components/HeaderElement/HeaderElement";
 import CurrencyConverter from "./components/CurrencyConverter/CurrencyConverter";
-import { useEffect, useState } from 'react';
+import useFetch from './getAPI/'
 
 function App() {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    // Объявляем асинхронную функцию внутри useEffect
-    async function fetchData() {
-      try {
-        // Выполняем API-запрос
-        const response = await fetch('https://v6.exchangerate-api.com/v6/e5cfbf2929bb755088a14a40/latest/USD');
-        // Преобразуем ответ в формат JSON
-        const data = await response.json();
-        // Обновляем состояние с полученными данными
-        setData(data);
-      } catch (error) {
-        console.error('Ошибка загрузки данных:', error);
-      }
-    }
-
-    // Вызываем асинхронную функцию
-    fetchData();
-  },[]);
-
-  console.log(data);
-  
+  const dataAPI = useFetch('RUB');
 
   return (
     <>
       <HeaderElement headerText='Конвертер валют' />
-      <CurrencyConverter />
+      <CurrencyConverter {...dataAPI}/>
       <p>На основании данных https://www.exchangerate-api.com/</p>
     </>
   )
