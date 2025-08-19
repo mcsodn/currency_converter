adc2d56831852c97ed550ce682ca7dfd2c65fbdc
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-08-19 20:58:06 +0500

    add start states

--- src/components/CurrencyBlock/CurrencyBlock.jsx
+++ src/components/CurrencyBlock/CurrencyBlock.jsx
@@ -1,12 +1,31 @@
 import './CurrencyBlock.css';
+import { useState } from "react";
+import convertDotToComma from '../../helper';
 
-const CurrencyBlock = () => {
+const CurrencyBlock = (props) => {
+    const [currencyValue, setCurrencyValue] = useState(() => {
+        return props.isMainCurrency ? '1' : props.data.conversion_rates[props.sec];
+    });
+
     return <div className='currency-block'>
-        <input type="text" name="" id="" value={'100,134'} />
+        <input 
+            type="text" 
+            name="" 
+            id="" 
+            value={convertDotToComma(currencyValue)}
+            onChange={(e) => setCurrencyValue(e.target.value)}
+        /> 
         <select name="" id="" >
-            <option value="eur">EUR</option>
+            
+            {Object.keys(props.data.conversion_rates).map(value => <option key={value} selected={!props.isMainCurrency && (value === props.sec) ? true : false}>{value}</option>)}
+            {/* todo: добавить пропс с базовой валютой с которой рендерится блок */}
+            {/* todo: к ней прикрутить базовую сумму и курс */}
         </select>
-        <p>course</p>
+        <p>{
+            props.isMainCurrency ? 
+            `1 ${props.main} = ${convertDotToComma(props.data.conversion_rates[props.sec])} ${props.sec}` : 
+            `1 ${props.sec} = ${convertDotToComma((1 / currencyValue).toFixed(4))} ${props.main}`
+        }</p>
     </div>
 }
 
