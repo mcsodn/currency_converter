2eeaf8808e8c669e4b94c25cea088e7ce5f13a01
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-08-19 20:57:01 +0500

    add data-props & mainCur flag

--- src/components/CurrencyConverter/CurrencyConverter.jsx
+++ src/components/CurrencyConverter/CurrencyConverter.jsx
@@ -1,12 +1,28 @@
 import './CurrencyConverter.css';
 import CurrencyBlock from "../CurrencyBlock/CurrencyBlock";
+import useFetch from '../../getAPI'
+import { useState } from "react";
 
 const CurrencyConverter = () => {
-    return <div className='currency-converter_block'>
-        <CurrencyBlock />
-        <button type="button"></button>
-        <CurrencyBlock />
-    </div>
+    const [currencyPair,setCurrencyPair] = useState({main: 'USD', sec: 'RUB'});
+    const dataAPI = useFetch(currencyPair.main);
+
+    if (dataAPI.loading) {
+        return <div className='currency-converter_block'>
+            <p>Загрузка...</p>
+        </div>
+    } else {
+        const CBProps = {
+            ...currencyPair,
+            ...dataAPI
+        };
+
+        return <div className='currency-converter_block'>
+            <CurrencyBlock isMainCurrency={true} {...CBProps}/>
+            <button type="button"></button>
+            <CurrencyBlock {...CBProps} />
+        </div>
+    }
 }
 
 export default CurrencyConverter;
