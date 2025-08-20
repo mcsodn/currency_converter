c1839af9bb1938d067b080c8588b47947830ea5e
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-08-20 22:58:39 +0500

    add calculation & value states

--- src/components/CurrencyBlock/CurrencyBlock.jsx
+++ src/components/CurrencyBlock/CurrencyBlock.jsx
@@ -1,25 +1,38 @@
 import './CurrencyBlock.css';
 import { useState } from "react";
 import formatDigitValue from '../../helper';
+import { useEffect } from 'react';
 
 const CurrencyBlock = (props) => {
-    const [currencyValue, setCurrencyValue] = useState(() => {
-        return props.isMainCurrency ? '1' : props.data.conversion_rates[props.sec];
-    });
+    const [currencyValue, setCurrencyValue] = useState(props.isMainCurrency ? props.mainCurrencyValue : props.secCurrencyValue);
+
+    useEffect(() => {
+        setCurrencyValue(props.isMainCurrency ? props.mainCurrencyValue : props.secCurrencyValue)
+    },[props])
 
     return <div className='currency-block'>
         <input 
             type="text"
+            id={props.isMainCurrency ? 'main-currency' : 'sec-currency'}
             value={formatDigitValue(currencyValue)}
-            onChange={(e) => setCurrencyValue(e.target.value)}
+            onChange={(e) => {
+                setCurrencyValue(e.target.value);
+                props.onChangeCurrencyValue(e);
+            }}
         /> 
-        <select value={!props.isMainCurrency && props.sec}>            
+        <select 
+            id={props.isMainCurrency ? 'main-currency' : 'sec-currency'} 
+            value={!props.isMainCurrency && props.secCurrency} 
+            onChange={(e) => {
+                props.onChangeCurrency(e);
+            }}
+        >
             {Object.keys(props.data.conversion_rates).map(value => <option key={value}>{value}</option>)}
         </select>
         <p>{
             props.isMainCurrency ? 
-            `1 ${props.main} = ${formatDigitValue(props.data.conversion_rates[props.sec])} ${props.sec}` : 
-            `1 ${props.sec} = ${formatDigitValue((1 / currencyValue).toFixed(4))} ${props.main}`
+            `1 ${props.mainCurrency} = ${formatDigitValue(props.data.conversion_rates[props.secCurrency])} ${props.secCurrency}` : 
+            `1 ${props.secCurrency} = ${formatDigitValue((1 / props.data.conversion_rates[props.secCurrency]).toFixed(4))} ${props.mainCurrency}`
         }</p>
     </div>
 }
--- src/components/CurrencyConverter/CurrencyConverter.jsx
+++ src/components/CurrencyConverter/CurrencyConverter.jsx
@@ -1,24 +1,62 @@
 import './CurrencyConverter.css';
 import CurrencyBlock from "../CurrencyBlock/CurrencyBlock";
-import useFetch from '../../getAPI'
+import useFetch from '../../getAPI';
 import { useState } from "react";
 
 const CurrencyConverter = () => {
-    const [currencyPair,setCurrencyPair] = useState({main: 'USD', sec: 'RUB'});
-    const dataAPI = useFetch(currencyPair.main);
+    const [currencyPair,setCurrencyPair] = useState({mainCurrency: 'USD', secCurrency: 'RUB'});
+    const [currencyValues,setCurrencyValues] = useState({mainCurrencyValue: '0', secCurrencyValue: '0'});
+    const dataAPI = useFetch(currencyPair.mainCurrency);
+    
+    const setCurrency = event => {        
+        setCurrencyPair(
+            event.target.id === 'main-currency' ? {
+                ...currencyPair,
+                mainCurrency: event.target.value
+            } : {
+                ...currencyPair,
+                secCurrency: event.target.value
+            }
+        );
+
+        setCurrencyValues(
+            event.target.id === 'main-currency' ? {
+                ...currencyValues,
+                secCurrencyValue: (currencyValues.mainCurrencyValue * dataAPI.data.conversion_rates[currencyPair.secCurrency]).toFixed(4)
+            } : {
+                ...currencyValues,
+                mainCurrencyValue: (currencyValues.secCurrencyValue / dataAPI.data.conversion_rates[currencyPair.secCurrency]).toFixed(4)
+            }
+        )
+    }
+
+    const setCurrencyValue = event => {
+        setCurrencyValues(
+            event.target.id === 'main-currency' ? {
+                mainCurrencyValue: event.target.value,
+                secCurrencyValue: (+event.target.value * dataAPI.data.conversion_rates[currencyPair.secCurrency]).toFixed(4)
+            } : {
+                mainCurrencyValue: (+event.target.value / dataAPI.data.conversion_rates[currencyPair.secCurrency]).toFixed(4),
+                secCurrencyValue: event.target.value
+            }
+        )
+    }
 
     if (dataAPI.loading) {
         return <div className='currency-converter_block'>
             <p>Загрузка...</p>
         </div>
-    } else {
+    } else {        
         const CBProps = {
             ...currencyPair,
-            ...dataAPI
+            ...currencyValues,
+            ...dataAPI,
+            onChangeCurrency: setCurrency,
+            onChangeCurrencyValue: setCurrencyValue
         };
 
         return <div className='currency-converter_block'>
-            <CurrencyBlock isMainCurrency={true} {...CBProps}/>
+            <CurrencyBlock isMainCurrency={true} {...CBProps} />
             <button type="button"></button>
             <CurrencyBlock {...CBProps} />
         </div>
