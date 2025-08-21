37d9efe8706b1bc0f6d384a622ebe25a9a6b991e
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-08-21 18:49:36 +0500

    delete API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css';
 import HeaderElement from "./components/HeaderElement/HeaderElement";
 import CurrencyConverter from "./components/CurrencyConverter/CurrencyConverter";
-import useFetch from './getAPI/'
 
 function App() {
-  const dataAPI = useFetch('RUB');
-
   return (
     <>
       <HeaderElement headerText='Конвертер валют' />
-      <CurrencyConverter {...dataAPI}/>
+      <CurrencyConverter />
       <p>На основании данных https://www.exchangerate-api.com/</p>
     </>
   )
