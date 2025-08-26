3c67db0332b15bb5d6608733d025fcfeed5a418c
Однолетков Максим Михайлович <odnoletkov.maksim@itmh.ru>
2025-08-26 15:57:04 +0500

    add link&style

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <HeaderElement headerText='Конвертер валют' />
       <CurrencyConverter />
-      <p>На основании данных https://www.exchangerate-api.com/</p>
+      <p>На основании данных <a href="https://www.exchangerate-api.com/" target='_blank'>https://www.exchangerate-api.com/</a></p>
     </>
   )
 }
